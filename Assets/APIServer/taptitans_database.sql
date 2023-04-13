01e7ada4d67637a6fc9319b71f492bcd4aee6a39
hygrintoht <francis_santonia@dlsu.edu.ph>
2023-04-14 03:48:01 +0800

    Update taptitans_database.sql

--- Assets/APIServer/taptitans_database.sql
+++ Assets/APIServer/taptitans_database.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS player (
     skill_points_owned  int NOT NULL DEFAULT 0,
     play_time TIME,
     last_played DATETIME,
-    installation_date DATETIME CURRENT_TIMESTAMP,
+    installation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT `PK_playerID_key` PRIMARY KEY (playerID),
     CONSTRAINT `FK_clanID_in_player_key` FOREIGN KEY (clanID)
 		REFERENCES clan(clanID)

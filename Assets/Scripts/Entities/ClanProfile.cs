8c3d25b4c88acd6364e6ed69d99a0708acb0b94b
Sir Sevenn <jewelmacuha@gmail.com>
2023-04-14 03:55:31 +0800

    Clan Profile Progress

--- Assets/Scripts/Entities/ClanProfile.cs
+++ Assets/Scripts/Entities/ClanProfile.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClanProfile : MonoBehaviour
 {
+    [Header("Clan Profile References")]
+    [SerializeField] private GameObject clanProfileTab;
+    [SerializeField] private TMP_Text clanIDText;
+    [SerializeField] private TMP_Text clanNameText;
+    [SerializeField] private TMP_Text inviteCode;
+    [SerializeField] private TMP_Text raidCompletedText;
+    [SerializeField] private TMP_Text clanMoraleText;
+    [SerializeField] private TMP_Text requiredStageText;
+    [SerializeField] private TMP_Text requiredRaidLevelText;
+    [SerializeField] private TMP_Text privacyText;
+
+    [Space(10)]
     public Clan clan;
+    public List<Player> clanMembers = new List<Player>();
+
+
     // Start is called before the first frame update
     void Start()
     {
